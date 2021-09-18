549d75db0ac408ab5a8a566269b0c3a7106f5431
Michael Housh <mhoush@houshhomeenergy.com>
2021-09-18 18:57:44 -0400

    Updated grains of moisture to be numeric.

--- Sources/GrainsOfMoisture/GrainsOfMoisture.swift
+++ Sources/GrainsOfMoisture/GrainsOfMoisture.swift
@@ -9,40 +9,46 @@ import Foundation
 /// Represents / calculates the grains of moisture for air.
 public struct GrainsOfMoisture: Equatable {
 
-  /// The altitude of the air.
-  public var altitude: Length
-
-  /// The temperature of the air.
-  public var temperature: Temperature
-
-  /// The relative humidity of the air.
-  public var humidity: RelativeHumidity
-
   /// Constant for the mole weight of water.
-  public let moleWeightWater = 18.02
+  public static let moleWeightWater = 18.02
 
   /// Constant for the mole weight of air.
-  public let moleWeightAir = 28.85
-
-  /// The vapor pressure of the air.
-  public var vaporPressure: Pressure {
-    .vaporPressure(at: temperature)
-  }
-
-  /// The ambient pressure of the air.
-  public var ambientPressure: Pressure {
-    .init(altitude: altitude)
-  }
-
-  /// The saturation humidity of the air.
-  public var saturationHumidity: Double {
-    7000 * (moleWeightWater / moleWeightAir) * vaporPressure.psi
+  public static let moleWeightAir = 28.85
+  
+  public static func saturationHumidity(vaporPressure: Pressure, ambientPressure: Pressure) -> Double {
+    7000 * (Self.moleWeightWater / Self.moleWeightAir) * vaporPressure.psi
       / (ambientPressure.psi - vaporPressure.psi)
   }
-
+  
+  private static func calculate(
+    _ temperature: Temperature,
+    _ humidity: RelativeHumidity,
+    _ pressure: Pressure
+  ) -> Double {
+    let vaporPressure = Pressure.vaporPressure(at: temperature)
+    let saturationHumidity = saturationHumidity(vaporPressure: vaporPressure, ambientPressure: pressure)
+    return saturationHumidity * humidity.fraction
+  }
+  
   /// The calculated grains per pound of air.
-  public var rawValue: Double {
-    saturationHumidity * humidity.fraction
+  public var rawValue: Double
+  
+  public init(_ value: Double) {
+    self.rawValue = value
+  }
+  
+  /// Create a new ``GrainsOfMoisture`` with the given temperature, humidity, and altitude.
+  ///
+  /// - Parameters:
+  ///   - temperature: The temperature of the air.
+  ///   - humidity: The relative humidity of the air.
+  ///   - pressure: The pressure of the air.
+  public init(
+    temperature: Temperature,
+    humidity: RelativeHumidity,
+    pressure: Pressure
+  ) {
+    self.rawValue = Self.calculate(temperature, humidity, pressure)
   }
 
   /// Create a new ``GrainsOfMoisture`` with the given temperature, humidity, and altitude.
@@ -56,11 +62,62 @@ public struct GrainsOfMoisture: Equatable {
     humidity: RelativeHumidity,
     altitude: Length = .seaLevel
   ) {
-    self.temperature = temperature
-    self.humidity = humidity
-    self.altitude = altitude
+    self.init(
+      temperature: temperature,
+      humidity: humidity,
+      pressure: .init(altitude: altitude)
+    )
   }
+}
 
+extension GrainsOfMoisture: Comparable {
+  public static func < (lhs: GrainsOfMoisture, rhs: GrainsOfMoisture) -> Bool {
+    lhs.rawValue < rhs.rawValue
+  }
+}
+
+extension GrainsOfMoisture: ExpressibleByFloatLiteral {
+  
+  public init(floatLiteral value: Double) {
+    self.init(value)
+  }
+}
+
+extension GrainsOfMoisture: ExpressibleByIntegerLiteral {
+  
+  public init(integerLiteral value: Int) {
+    self.init(floatLiteral: Double(value))
+  }
+}
+
+extension GrainsOfMoisture: AdditiveArithmetic {
+  public static func - (lhs: GrainsOfMoisture, rhs: GrainsOfMoisture) -> GrainsOfMoisture {
+    .init(lhs.rawValue - rhs.rawValue)
+  }
+  
+  public static func + (lhs: GrainsOfMoisture, rhs: GrainsOfMoisture) -> GrainsOfMoisture {
+    .init(lhs.rawValue + rhs.rawValue)
+  }
+}
+
+extension GrainsOfMoisture: Numeric {
+  public init?<T>(exactly source: T) where T : BinaryInteger {
+    self.init(Double(source))
+  }
+  
+  public var magnitude: Double.Magnitude {
+    rawValue.magnitude
+  }
+  
+  public static func * (lhs: GrainsOfMoisture, rhs: GrainsOfMoisture) -> GrainsOfMoisture {
+    .init(lhs.rawValue * rhs.rawValue)
+  }
+  
+  public static func *= (lhs: inout GrainsOfMoisture, rhs: GrainsOfMoisture) {
+    lhs.rawValue *= rhs.rawValue
+  }
+  
+  public typealias Magnitude = Double.Magnitude
 }
 
 extension Temperature {
--- Tests/GrainsOfMoistureTests/GrainsOfMoistureTests.swift
+++ Tests/GrainsOfMoistureTests/GrainsOfMoistureTests.swift
@@ -10,4 +10,27 @@ final class GrainsOfMoistureTests: XCTestCase {
     let grains = temperature.grains(humidity: 50%)
     XCTAssertEqual(round(grains.rawValue * 100) / 100, 65.9)
   }
+  
+  func test_comparable() {
+    XCTAssertTrue(GrainsOfMoisture.init(exactly: 28)! > 26)
+  }
+  
+  func test_addition_and_subtraction() {
+    var grains = GrainsOfMoisture.init(10)
+    grains += 10
+    XCTAssertEqual(grains.rawValue, 20)
+    grains -= 5
+    XCTAssertEqual(grains.rawValue, 15)
+  }
+  
+  func test_magnitude() {
+    XCTAssertEqual(GrainsOfMoisture.init(10).magnitude, 10.magnitude)
+  }
+  
+  func test_multiplication() {
+    var grains: GrainsOfMoisture = 10 * 3
+    XCTAssertEqual(grains.rawValue, 30)
+    grains *= 2
+    XCTAssertEqual(grains.rawValue, 60)
+  }
 }
