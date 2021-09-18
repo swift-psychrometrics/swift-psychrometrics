0526f2e38eec2e49dd110e47beb13de7cb09a87e
Michael Housh <mhoush@houshhomeenergy.com>
2021-09-18 09:29:42 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-psychrometrics
+
+A description of this package.
---
+++ Sources/swift-psychrometrics/swift_psychrometrics.swift
@@ -0,0 +1,6 @@
+public struct swift_psychrometrics {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
