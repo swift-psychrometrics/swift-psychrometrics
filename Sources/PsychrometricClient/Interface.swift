90382d69d6049cd9140a685f525a61174ab8338a
Michael Housh <mhoush@houshhomeenergy.com>
2023-09-05 12:51:58 -0400

    wip

--- Sources/PsychrometricClient/Interface.swift
+++ Sources/PsychrometricClient/Interface.swift
@@ -4,16 +4,66 @@ import SharedModels
 /// Performs calculations for different psychrometric properties.
 public struct PsychrometricClient {
 
+  /// Perform density calculations / conversions.
   public var density: DensityClient
+
+  /// Perform dew-point temperature calculations / conversions.
   public var dewPoint: @Sendable (DewPointRequest) async -> DewPoint
+
+  /// Perform enthalpy calculations / conversions.
   public var enthalpy: Enthalpy
+
+  /// Perform grains of moisture calculations / conversions.
   public var grainsOfMoisture: @Sendable (GrainsOfMoistureRequest) async throws -> GrainsOfMoisture
+
+  /// Perform humidity ratio calculations / conversions.
   public var humidityRatio: @Sendable (HumidityRatioRequest) async throws -> HumidityRatio
+
+  /// Perform relative humidity calculations / conversions.
   public var relativeHumidity: @Sendable (RelativeHumidityRequest) async throws -> RelativeHumidity
+
+  /// Perform saturation pressure calculations / conversions.
   public var saturationPressure: @Sendable (SaturationPressureRequest) async throws -> SaturationPressure
+
+  /// Perform specific heat calculations / conversions.
   public var specificHeat: SpecificHeatClient
+
+  /// Perform specific humidity calculations / conversions.
   public var specificHumidity: @Sendable (SpecificHumidityRequest) async -> SpecificHumidity
 
+  /// Perform specific volume calculations / conversions.
+  public var specificVolume: SpecificVolumeClient
+
+  /// Perform vapor pressure calculations / conversions.
+  public var vaporPressure: (VaporPressureRequest) async throws -> VaporPressure
+
+  public init(
+    density: DensityClient, 
+    dewPoint: @escaping @Sendable (DewPointRequest) -> DewPoint,
+    enthalpy: Enthalpy,
+    grainsOfMoisture: @escaping @Sendable (GrainsOfMoistureRequest) async throws -> GrainsOfMoisture,
+    humidityRatio: @escaping @Sendable (HumidityRatioRequest) async throws -> HumidityRatio,
+    relativeHumidity: @escaping @Sendable (RelativeHumidityRequest) async throws -> RelativeHumidity,
+    saturationPressure: @escaping @Sendable (SaturationPressureRequest) async throws -> SaturationPressure,
+    specificHeat: SpecificHeatClient,
+    specificHumidity: @escaping @Sendable (SpecificHumidityRequest) async -> SpecificHumidity,
+    specificVolume: SpecificVolumeClient,
+    vaporPressure: @escaping @Sendable (VaporPressureRequest) async throws -> VaporPressure
+  ) {
+    self.density = density
+    self.dewPoint = dewPoint
+    self.enthalpy = enthalpy
+    self.grainsOfMoisture = grainsOfMoisture
+    self.humidityRatio = humidityRatio
+    self.relativeHumidity = relativeHumidity
+    self.saturationPressure = saturationPressure
+    self.specificHeat = specificHeat
+    self.specificHumidity = specificHumidity
+    self.specificVolume = specificVolume
+    self.vaporPressure = vaporPressure
+  }
+
+  /// Perform density calculations.
   public struct DensityClient {
 
     /// Calculate the density of dry air for the given request.
@@ -164,7 +214,7 @@ public struct PsychrometricClient {
     public var water: @Sendable (Temperature) async -> SpecificHeat
 
     public init(
-      water: @escaping @Sendable (Temperature) -> SpecificHeat
+      water: @escaping @Sendable (Temperature) async -> SpecificHeat
     ) {
       self.water = water
     }
@@ -176,7 +226,47 @@ public struct PsychrometricClient {
   }
 
   public struct SpecificVolumeClient {
-    public var dryAir: @Sendable () async -> SpecificVolumeOf<DryAir>
-    public var moistAir: @Sendable () async -> SpecificVolumeOf<MoistAir>
+    public var dryAir: @Sendable (DryAirRequest) async -> SpecificVolumeOf<DryAir>
+    public var moistAir: @Sendable (MoistAirRequest) async throws -> SpecificVolumeOf<MoistAir>
+
+    public struct DryAirRequest: Equatable, Sendable {
+      public let dryBulb: DryBulb
+      public let totalPressure: TotalPressure
+      public let units: PsychrometricUnits?
+
+      public init(
+        dryBulb: DryBulb,
+        totalPressure: TotalPressure,
+        units: PsychrometricUnits? = nil
+      ) {
+        self.dryBulb = dryBulb
+        self.totalPressure = totalPressure
+        self.units = units
+      }
+    }
+
+    public struct MoistAirRequest: Equatable, Sendable {
+      public let dryBulb: DryBulb
+      public let humidityRatio: HumidityRatio
+      public let totalPressure: TotalPressure
+      public let units: PsychrometricUnits?
+
+      public init(
+        dryBulb: DryBulb,
+        humidityRatio: HumidityRatio,
+        totalPressure: TotalPressure,
+        units: PsychrometricUnits? = nil
+      ) {
+        self.dryBulb = dryBulb
+        self.humidityRatio = humidityRatio
+        self.totalPressure = totalPressure
+        self.units = units
+      }
+    }
+  }
+
+  public enum VaporPressureRequest: Equatable, Sendable {
+    case humidityRatio(HumidityRatio, totalPressure: TotalPressure, units: PsychrometricUnits? = nil)
+    case relativeHumidity(RelativeHumidity, dryBulb: DryBulb, units: PsychrometricUnits? = nil)
   }
 }
